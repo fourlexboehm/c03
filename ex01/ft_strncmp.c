334b90f0185104ecc887f83e794be438d09a271b
Alex Boehm <alexboehm@systemli.org>
2021-07-29 12:12:44 +0930

    fixed

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -3,16 +3,14 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int	i;
 
 	i = 0;
-	while (s1[i] == s2[i] && i < n && (s1[i] != '\0' || s2[i] != '\0'))
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i < n -1)
 		i++;
 	return (s1[i] - s2[i]);
 }
-/*
 #include <stdio.h>
 int	main(void)
 {
-	char	s1[] = "1999";
+	char	s1[] = "1099";
 	char	s2[] = "1551";
 	printf("%d", ft_strncmp(s1, s2, 4)); 
 }
-*/
